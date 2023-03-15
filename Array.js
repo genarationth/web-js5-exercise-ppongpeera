1903d5fea32639cbb46d7fda20473763b5a538f8
ppongpeera <ppongpeera@gmail.com>
2023-03-16 00:30:14 +0700

    first commit but undone

---
+++ Array.js
@@ -0,0 +1,45 @@
+//Exercise #1
+
+let waiting = ['Sofia', 'David', 'Juan']
+
+waiting.push('Sara', 'Augustin');
+console.log(waiting);
+
+waiting.splice(2, 0, 'Renata')
+waiting.push('Elena')
+console.log(waiting);
+
+
+//Exercise #2
+
+
+
+
+
+
+//Exercise #3
+
+for (let x = 20; x > 0; x-=0.5) {
+    console.log(x);
+   }
+
+//odd Number
+for (let i = 1; i < 100; i+=2) {
+    console.log(i);
+   }
+
+
+// [n]
+
+let n = 6
+let i = 1
+while (i <= n) {
+    console.log([i]);
+    i++;
+}
+
+//sum
+
+const n = 19
+let i = 1
+
